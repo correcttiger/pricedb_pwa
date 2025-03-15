44ac38dfd893ea6c7bfea75dc0695d8976da8007
correcttiger <123797025+correcttiger@users.noreply.github.com>
2025-03-16 08:28:57 +0900

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+navigator.serviceWorker?.register("/service-worker.js").then(registration => {
+    console.log("Service Worker registered with scope:", registration.scope);
+}).catch(error => {
+    console.error("Service Worker registration failed:", error);
+});
---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("my-cache").then((cache) => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/manifest.json",
+                "/scripts/app.js",
+                "/bootstrap/bootstrap.min.css",
+                "/bootstrap/bootstrap.bundle.min.js",
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
