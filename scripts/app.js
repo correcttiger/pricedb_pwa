3e57cf140d5bf5e22f27de8ce633f8bf17406a50
correcttiger <123797025+correcttiger@users.noreply.github.com>
2025-03-16 09:27:14 +0900

    10

--- scripts/app.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("scripts/service-worker.js").then(registration => {
-        console.log("Service Worker registered with scope:", registration.scope);
-    }).catch(error => {
-        console.error("Service Worker registration failed", error);
-    });
-}
